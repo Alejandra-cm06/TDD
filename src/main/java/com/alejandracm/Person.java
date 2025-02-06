64f8b70a983f80d3822c489116c2342d9fc8cae1
Alejandra Cordeiro Mourelle <aleco.mo06@gmail.com>
2025-02-06 12:53:44 +0100

    06/02/2025

---
+++ src/main/java/com/alejandracm/Person.java
@@ -0,0 +1,41 @@
+package com.alejandracm;
+
+public class Person {
+    private String Name;
+    private int  Age;
+    private String Gender;
+
+
+    public Person(String name, int age, String gender) {
+        this.Name = name;
+        this.Age = age;
+        this.Gender = gender;
+    }
+
+
+    public String getName() {
+        return Name;
+    }
+    public int getAge() {
+        return Age;
+    }
+    public String getGender() {
+        return Gender;
+    }
+
+
+    public void setName(String name) {
+        Name = name;
+    }
+    public void setAge(int age) {
+        Age = age;
+    }
+    public void setGender(String gender) {
+        Gender = gender;
+    }
+
+
+    
+    
+
+}
